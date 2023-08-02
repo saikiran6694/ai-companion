163aca6724131898aeaf2bf2f6f77c928345d477
saikiranbembalge <saikiranbembalge123@gmail.com>
2023-08-02 19:13:52 +0530

    first commit

---
+++ ai-companion/components/bot-avatar.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+
+interface BotAvatarProps {
+  src: string;
+}
+
+export const BotAvatar = ({ src }: BotAvatarProps) => {
+  return (
+    <Avatar className="h-8 w-8">
+      <AvatarImage src={src} />
+    </Avatar>
+  );
+};
